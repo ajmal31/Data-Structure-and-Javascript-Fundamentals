e5ab223a4d1773ee37aeea6d0770af227ceed4ab
ajmal31 <ajmalmuhammed846@gmail.com>
2023-09-27 23:31:33 +0530

    leetcode question number 1389

---
+++ Leetcode/1389.js
@@ -0,0 +1,8 @@
+var createTargetArray = function(nums, index) {
+    let target=[]
+    for(let i=0;i<nums.length;i++)
+        {
+            target.splice(index[i],0,nums[i])
+        }
+    return target
+};
