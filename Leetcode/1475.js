463e6e1756c4e6402fe02669233a90212a21494f
ajmal31 <ajmalmuhammed846@gmail.com>
2023-10-04 07:08:07 +0530

    leetcode question 1475 commit 15

---
+++ Leetcode/1475.js
@@ -0,0 +1,19 @@
+var finalPrices = function(arr) {
+    
+  
+    for(let i=0;i<arr.length;i++)
+        {
+            for(let j=i+1;j<arr.length;j++)
+                {
+                    if(arr[i]>=arr[j])
+                        {
+                            arr[i]=arr[i]-arr[j]
+                            break;
+                        }
+                }
+                   
+                    
+        }
+    return arr
+    
+};
