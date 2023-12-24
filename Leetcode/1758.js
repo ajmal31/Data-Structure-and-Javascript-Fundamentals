590baf6fad47290240bbcc68f94bd8d2dabf3829
ajmal31 <ajmalmuhammed846@gmail.com>
2023-12-24 23:39:27 +0530

    Refactored : leetcode question 1758 v83

--- Leetcode/1758.js
+++ Leetcode/1758.js
@@ -20,3 +20,25 @@ var minOperations = function(s) {
    return Math.min(count1, count2);
 
 };
+
+//Approach two
+
+var minOperations = function (s) {
+   let count = 0
+   let count2=0
+
+   for (let i = 0; i < s.length; i++) {
+
+           if (i % 2 === 0 && s[i] !== "0") count++
+           if (i % 2 !== 0 && s[i] !== "1") count++
+
+           if (i % 2 === 0 && s[i] !== "1") count2++
+           if (i % 2 !== 0 && s[i] !== "0") count2++
+
+       
+   }
+   return Math.min(count,count2)
+
+
+
+};
