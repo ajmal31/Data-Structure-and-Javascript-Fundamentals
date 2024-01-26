1c4171d06183add9ae8cb45eb104313724e6dd9b
ajmal31 <ajmalmuhammed846@gmail.com>
2024-01-27 01:21:10 +0530

    leetcode question 2810 109

---
+++ Leetcode/2810.js
@@ -0,0 +1,12 @@
+var finalString = function(s) {
+    let result=''
+    for(let i=0;i<s.length;i++)
+    {
+        if(s[i]==="i") result=result.split('').reverse('').join('')
+        else result+=s[i]
+    }
+    return result
+   
+    
+    
+};
