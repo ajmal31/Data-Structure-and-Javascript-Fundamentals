50bf13b115be2ae953b9b55e8c2390949fdb4108
ajmal31 <ajmalmuhammed846@gmail.com>
2024-01-30 13:46:09 +0530

    leetcode question 2839 v111

---
+++ Leetcode/2839.js
@@ -0,0 +1,9 @@
+var canBeEqual = function(s1, s2) {
+
+    if(s1===s2)return true
+    const [a,b,c,d]=s1
+    if(a+d+c+b===s2||c+b+a+d===s2||c+d+a+b===s2)return true
+    else return false
+      
+      
+  };
