3ae2b61819231574146a9040abc0097feb223b69
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-01 01:47:11 +0530

    leetcode question 3010 v112

---
+++ Leetcode/3010.js
@@ -0,0 +1,17 @@
+var minimumCost = function (nums) {
+
+    let min1 = Infinity
+    let min2 = Infinity
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < min1) {
+
+            min2 = min1
+            min1 = nums[i]
+
+        }
+        else if (nums[i] < min2) min2 = nums[i]
+    }
+    return nums[0] + min1 + min2
+
+};
