209668fac015fb57473a8d18751049ebded2d32b
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-23 21:50:23 +0530

    leetcode question 347 v133

---
+++ Leetcode/347.js
@@ -0,0 +1,21 @@
+var topKFrequent = function(nums, k) {
+
+    let map=new Map()
+    let result=[]
+    for(const key of nums)
+    {
+        map.set(key,map.get(key)+1||1)
+    }
+
+    let arr=[...map.values()]
+    arr.sort((a,b)=>b-a)
+    arr=arr.slice(0,k)
+    
+    for(const key of map){
+
+        if(arr.includes(key[1]))result.push(key[0])
+    }
+    return result
+    
+ 
+};
