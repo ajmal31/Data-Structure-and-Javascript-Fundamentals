24dfbd9bf032440b3178b6164e0cc0c271596ac1
ajmal31 <ajmalmuhammed846@gmail.com>
2024-03-05 10:00:36 +0530

    leetcode question 3042 v143

---
+++ Leetcode/3042.js
@@ -0,0 +1,12 @@
+var countPrefixSuffixPairs = function(words) {
+    let count=0
+    for(let i=0;i<words.length;i++)
+    {
+        for(let j=i+1;j<words.length;j++)
+        {
+            if(words[j].startsWith(words[i])&&words[j].endsWith(words[i]))count++
+        }
+    
+    }
+    return count
+};
