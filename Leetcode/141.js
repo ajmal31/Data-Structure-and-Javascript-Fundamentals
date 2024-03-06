dfdf027366baa290ac440151c728f34428294ff2
ajmal31 <ajmalmuhammed846@gmail.com>
2024-03-06 13:29:15 +0530

    leetcode question 143

---
+++ Leetcode/141.js
@@ -0,0 +1,15 @@
+var hasCycle = function(head) {
+
+   let slower=head
+   let faster=head
+
+   while(faster&&faster.next){
+       slower=slower.next
+       faster=faster.next.next
+       if(slower===faster)return true
+
+   }
+   return false
+   
+    
+}
