c7e5bbaae64d38651e49f74b21f2a41cdf69a7c5
ajmal31 <ajmalmuhammed846@gmail.com>
2024-03-24 00:00:11 +0530

    kjshf

---
+++ Tree/traversal.js
@@ -0,0 +1,102 @@
+class Node{
+    constructor(val){
+        this.val=val
+        this.left=null
+        this.right=null
+    }
+}
+
+class Bst{
+    constructor(){
+        this.root=null
+    }
+    insert(val){
+        
+        let newNode= new Node(val)
+        if(!this.root)this.root=newNode
+        else{
+            this.insertNode(this.root,newNode)
+        }
+        
+    }
+    insertNode(root,newNode){
+        
+             if(root.val<newNode.val)
+             {
+                 if(!root.right) root.right=newNode
+                 else this.insertNode(root.right,newNode)
+             }else {
+                 
+                 if(!root.left) root.left=newNode
+                 else this.insertNode(root.left,newNode)
+             }
+        
+    }
+    preOrder(root)
+    {
+        if(root)
+        {
+            console.log(root.val)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+        
+    }
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.val)
+            this.inOrder(root.right)
+            
+        }
+    }
+    postOrder(root)
+    {
+        if(root)
+        {
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.val)
+        }
+    }
+    BFS(){
+    
+        let queue=[]
+        let visited=[]
+        let current=this.root
+        queue.push(current)
+        while(queue.length)
+        {
+            current=queue.shift()
+            visited.push(current.val)
+
+            current.left&&queue.push(current.left)
+            current.right&&queue.push(current.right)
+        }
+
+        console.log("visted ",visited)
+        return 
+        
+    }
+}
+
+const obj=new Bst()
+obj.insert(10)
+obj.insert(5)
+obj.insert(35)
+obj.insert(3)
+obj.insert(8)
+obj.insert(30)
+obj.insert(40)
+obj.insert(2)
+obj.insert(4)
+obj.insert(7)
+obj.insert(9)
+obj.insert(28)
+obj.insert(32)
+obj.insert(37)
+obj.insert(49)
+// obj.preOrder(obj.root)
+// obj.inOrder(obj.root)
+// obj.postOrder(obj.root)
+obj.BFS()
